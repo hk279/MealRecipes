79e79961bf147e1af400b468876de72052c7eeb1
Jere Järvinen <jerejarvinen94@gmail.com>
2019-11-25 16:54:05 +0200

    Dropdown selection for category and list view working.

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, ListGroup, Dropdown, DropdownButton } from 'react-bootstrap'
+import { Container, ListGroup, Dropdown, DropdownButton, Jumbotron } from 'react-bootstrap'
 
 class CategoriesDropdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
         categories: [],
-        selectedCategory: "",
-        selectedMainIngredient: ""
     };
-    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -37,26 +34,26 @@ class CategoriesDropdown extends React.Component {
         });
     }
 
-    sendData = () => {
-      this.props.parentCallback(this.state.selectedCategory);
-    }
-
-    handleClick() {
-      console.log('hello')
+    sendData = (selectedCategory) => {
+      this.props.parentCallback(selectedCategory);
     }
 
   render() {
+    var selectedCategory = ""
     return(
       <Container>
-        <DropdownButton id="dropdown-basic-button" title="Categories">
-          {this.state.categories.map(item => (
-            <Dropdown.Item 
-            key={item.strCategory} 
-            onClick={() => {this.setState({selectedCategory: item.strCategory}); this.sendData()}}>
-              {item.strCategory}
-            </Dropdown.Item>
-          ))}
-        </DropdownButton>
+        <Jumbotron>
+          <h2>Select a category</h2>
+          <DropdownButton id="categories-dropdown" title="Categories">
+            {this.state.categories.map(item => (
+              <Dropdown.Item 
+              key={item.strCategory} 
+              onClick={() => {selectedCategory = item.strCategory; this.sendData(selectedCategory)}}>
+                {item.strCategory}
+              </Dropdown.Item>
+            ))}
+          </DropdownButton>
+        </Jumbotron>
       </Container>
     );
   }
@@ -71,46 +68,72 @@ class List extends React.Component {
     }
 
     componentDidMount() {
-      var selectedCategory = this.props.filter
-      var url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + selectedCategory
-      if (url === "https://www.themealdb.com/api/json/v1/1/filter.php?c=") {
-        url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
-      }
-      console.log(url)
-
-      //A necessary workaround to make setState work later.
-      const that = this
-      fetch(url)
-        .then(
-          function(response) {
-              if (response.status !== 200) {
-                console.log('Looks like there was a problem. Status Code: ' +
-                  response.status);
-                return;
-              }
-              response.json().then(function(data) {
-                that.setState({
-                  filteredMeals: data.meals
+        var url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + this.props.filter
+        console.log(url)
+  
+        //A necessary workaround to make setState work later.
+        const that = this
+        fetch(url)
+          .then(
+            function(response) {
+                if (response.status !== 200) {
+                  console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                  return;
+                }
+                response.json().then(function(data) {
+                  that.setState({
+                    filteredMeals: data.meals
+                  });
+                  console.log(data)
                 });
-                console.log(data)
-              });
-            }
-          )
-          .catch(function(err) {
-            console.log('Fetch Error :-S', err);
-          });
+              }
+            )
+            .catch(function(err) {
+              console.log('Fetch Error :-S', err);
+            });
+    }
+
+    componentDidUpdate(prevProps) {
+      if (this.props.filter !== prevProps.filter) {
+        var url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=" + this.props.filter
+        console.log(url)
+  
+        //A necessary workaround to make setState work later.
+        const that = this
+        fetch(url)
+          .then(
+            function(response) {
+                if (response.status !== 200) {
+                  console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                  return;
+                }
+                response.json().then(function(data) {
+                  that.setState({
+                    filteredMeals: data.meals
+                  });
+                  console.log(data)
+                });
+              }
+            )
+            .catch(function(err) {
+              console.log('Fetch Error :-S', err);
+            });
       }
+    }
 
     render() {
-    return(
-        <Container>
-            <ListGroup variant="flush">
+      return(
+          <Container>
+            <h2>{this.props.filter}</h2>
+            <ListGroup id="meals-list" variant="flush">
               {this.state.filteredMeals.map(item => (
-                <ListGroup.Item key={item.strMeal}>{item.strMeal}</ListGroup.Item>
+                <ListGroup.Item action key={item.strMeal}>{item.strMeal}</ListGroup.Item>
               ))}
             </ListGroup>
-        </Container>
-        )
+          </Container>
+          )
     }
 }
 
@@ -127,12 +150,21 @@ class Browse extends React.Component {
   }
 
   render() {
-  return(
-      <Container>
-        <CategoriesDropdown parentCallback = {this.callbackFunction} />
-        <List filter = {this.state.selectedCategory} />
-      </Container>
+    console.log(this.state.selectedCategory)
+    if (this.state.selectedCategory !== "") {
+      return(
+        <Container>
+          <CategoriesDropdown parentCallback = {this.callbackFunction} />
+          <List filter = {this.state.selectedCategory} />
+        </Container>
+        )
+    } else {
+      return(
+        <Container>
+          <CategoriesDropdown parentCallback = {this.callbackFunction} />
+        </Container>
       )
+    }
   }
 }
 
