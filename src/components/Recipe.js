9afe9d2b6a99bd0bd9c8c8f0678e41ffa3f93b57
Jere Järvinen <jerejarvinen94@gmail.com>
2019-11-28 16:04:36 +0200

    Added ingredients and a picture to the browse view

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 class Recipe extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             mealName: "",
-            mealInstructions: ""
+            mealInstructions: "",
+            mealIngredients: [],
+            mealPictureThumb: ""
         };
     }
 
@@ -25,10 +27,38 @@ class Recipe extends React.Component {
                         return;
                     }
                     response.json().then(function(data) {
+                        var ingredientsNameAndMeasure = []
+                        var meal = data.meals[0]
+
+                        ingredientsNameAndMeasure.push(
+                            [meal.strIngredient1, meal.strMeasure1],
+                            [meal.strIngredient2, meal.strMeasure2],
+                            [meal.strIngredient3, meal.strMeasure3],
+                            [meal.strIngredient4, meal.strMeasure4],
+                            [meal.strIngredient5, meal.strMeasure5],
+                            [meal.strIngredient6, meal.strMeasure6],
+                            [meal.strIngredient7, meal.strMeasure7],
+                            [meal.strIngredient8, meal.strMeasure8],
+                            [meal.strIngredient9, meal.strMeasure9],
+                            [meal.strIngredient10, meal.strMeasure10],
+                            [meal.strIngredient11, meal.strMeasure11],
+                            [meal.strIngredient12, meal.strMeasure12],
+                            [meal.strIngredient13, meal.strMeasure13],
+                            [meal.strIngredient14, meal.strMeasure14],
+                            [meal.strIngredient15, meal.strMeasure15],
+                            [meal.strIngredient16, meal.strMeasure16],
+                            [meal.strIngredient17, meal.strMeasure17],
+                            [meal.strIngredient18, meal.strMeasure18],
+                            [meal.strIngredient19, meal.strMeasure19],
+                            [meal.strIngredient20, meal.strMeasure20]
+                        )
+                    
                         that.setState(
                             {
-                                mealName: data.meals[0].strMeal,
-                                mealInstructions: data.meals[0].strInstructions
+                                mealName: meal.strMeal,
+                                mealInstructions: meal.strInstructions,
+                                mealIngredients: ingredientsNameAndMeasure,
+                                mealPictureThumb: meal.strMealThumb
                             })
                     });
                     }
@@ -42,7 +72,26 @@ class Recipe extends React.Component {
     render() {
         return(
             <Container>
-                {this.state.mealInstructions}
+                <Row>
+                    <Col>
+                        <ul>
+                            {this.state.mealIngredients.map((item, i) => 
+                                {if(item[0] !== "") {
+                                    return(<li key={i}>{item[1] + " " + item[0]}</li>)
+                                }
+                                else {
+                                    return(null)
+                                }}
+                            )}
+                        </ul>
+                    </Col>
+                    <Col>
+                        <img className="meal" src={this.state.mealPictureThumb} alt="Meal"></img>
+                    </Col>
+                </Row>
+                <Row id="instructions">
+                    <Col>{this.state.mealInstructions}</Col>
+                </Row>
             </Container>
         )
     }
